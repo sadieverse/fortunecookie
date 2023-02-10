71b9d3ef8fb2a2ad8b7dbe8d70dd570c1bb818ce
Saida Sainova <118192371+sadieverse@users.noreply.github.com>
2023-02-10 10:10:29 +0600

    Add files via upload

--- app.js
+++ app.js
@@ -2,32 +2,57 @@ document.getElementById('headText').innerHTML = 'Click on the button to get your
 document.getElementById('bottomText').innerHTML = `made by @sadieverse`;
 document.getElementById('button').innerHTML = 'Get your message';
 
-function getData() {
+// function getData() {
+//     let n = Math.floor(Math.random() * 27)
+//     let result = fetch(`http://my-json-server.typicode.com/sadieverse/cookieserver/messages/${n}`)
+//     .then(res => JSON.parse(res.value))
+//     .catch(err =>{'Up to no good today('});
+//     message = `${result}`;
+//     console.log(message);
+// }
+
+
+// async function getData() {
+//     let n = Math.floor(Math.random() * 27)
+//     let result = await fetch(`http://my-json-server.typicode.com/sadieverse/cookieserver/messages/${n}`)
+//     .then(res => res.json())
+//     .then(result => {return result["title"]})
+//     .catch(err =>{'Up to no good today('});
+//     message = `${result}`;
+//     console.log(result);
+// }
+
+
+// function getMessage() {
+//     const newMes = document.createElement("div");
+//     document.body.prepend(newMes);
+//     newMes.innerHTML = '<p id="yourMessage"></p>';
+//     document.getElementById("yourMessage").innerHTML = message;
+// }
+   
+
+async function getData() {
     let n = Math.floor(Math.random() * 27)
-    let result = fetch(`http://my-json-server.typicode.com/sadieverse/cookieserver/messages/${n}`)
-    .catch(error =>{'Up to no good today('});
-    message = result.getString("title");
-    console.log(message);
-}
-function getMessage() {
+    let result = await fetch(`http://my-json-server.typicode.com/sadieverse/cookieserver/messages/${n}`)
+    .then(res => res.json())
+    .then(result => {return result["title"]})
+    .catch(err =>{'Up to no good today('});
+    message = `${result}`;
+    console.log(result);
     const newMes = document.createElement("div");
-    document.body.appendChild(newMes);
+    document.body.prepend(newMes);
     newMes.innerHTML = '<p id="yourMessage"></p>';
     document.getElementById("yourMessage").innerHTML = message;
 }
-   
 
 
 
+// function getMessage() {
+//     const newMes = document.createElement("div");
+//     document.body.prepend(newMes);
+//     newMes.innerHTML = '<p id="yourMessage"></p>';
+//     document.getElementById("yourMessage").innerHTML = message;
+// }
 
-// function randomMessage(message)
-//     const random = Math.floor(Math.random() * 51);
-    
-//     let entity;
-//     const card = document.createElement("div");
-//     card.classList.add("card", cardSuit.toLowerCase());
-//     card.innerHTML = '<span class="card-value-suit top">' + cardValue + entity + "</span>" + '<span class="card-suit">' + entity + "</span>" + '<span class="card-value-suit bot">' + cardValue + entity + "</span>";
-//     document.body.appendChild(card);
-//   }
-//   const cards = deckBuilder();
-//   randomCard(cards);
+
+
